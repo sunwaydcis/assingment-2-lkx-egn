b8f1c0166d00e37da72a4d753b1b39494f450e51
Egan <eganyap@hotmail.com>
2025-11-24 17:57:42 +0800

    Implemented the file reading logic...

--- src/main/scala/HotelAnalysis.scala
+++ src/main/scala/HotelAnalysis.scala
@@ -1,6 +1,7 @@
 import scala.util.{Try, Success, Failure}
 import scala.io.{Source, Codec}
 import scala.util.{Try, Success, Failure}
+import java.nio.charset.CodingErrorAction
 
 //Define the Data Model
 case class Booking(
@@ -16,4 +17,46 @@ case class Booking(
 
 object HotelAnalysis {
 
+  //File Reading logic
+  def loadBookingData(filePath: String): List[Booking] = {
+    try {
+      implicit val codec: Codec = Codec("UTF-8")
+      codec.onMalformedInput(CodingErrorAction.REPLACE)
+      codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+
+      val source = Source.fromFile(filePath)
+      val lines = source.getLines().toList
+      source.close()
+
+      lines.drop(1).flatMap { line =>
+
+        val cols = line.split(",").map(_.trim)
+
+        Try {
+
+          if (cols.length < 24) throw new Exception("Incomplete row")
+          val price = cols(20).toDouble
+          val discountStr = cols(21).replace("%", "")
+          val discount = if (cols(21).contains("%")) discountStr.toDouble / 100 else discountStr.toDouble
+          val margin = cols(23).toDouble
+
+          Booking(
+            bookingId = cols(0),
+            customerOrigin = cols(6),
+            destinationCountry = cols(9),
+            hotelName = cols(16),
+            bookingPrice = price,
+            discount = discount,
+            profitMargin = margin,
+            visitors = cols(11).toInt
+          )
+        }.toOption
+      }
+    } catch {
+      case e: Exception =>
+        println(s"Error reading file: ${e.getMessage}")
+        e.printStackTrace()
+        List.empty[Booking]
+    }
+  }
 }
